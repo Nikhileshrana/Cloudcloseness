3283536e7c91a79dd9597064c554f67f9c059338
Nikhilesh Rana <101921995+Nikhileshrana@users.noreply.github.com>
2023-11-23 15:06:11 +0530

    Update sender.js

--- sender/sender.js
+++ sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("ws://192.168.203.67:3000")
+const webSocket = new WebSocket("ws://https://cloudcloseness.onrender.com:3000")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
