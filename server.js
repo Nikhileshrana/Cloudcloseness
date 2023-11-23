fda11b4c54eb66fead743e99907c1625f6bb977f
Nikhilesh Rana <101921995+Nikhileshrana@users.noreply.github.com>
2023-11-23 15:08:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => { })
 
-server.listen(3000, () => {
+server.listen(10000, () => {
     console.log("Hey , Nikhilesh Server is Running Sucessfully!!!!!")
 })
 
